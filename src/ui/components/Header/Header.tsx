eb320aecc6f7ba4a9d078c1eaf107be2f2225e75
Gabriel Teodoro <oi@gabrielteodoro.com>
2021-09-23 14:11:35 -0300

    chore: add Header component

---
+++ src/ui/components/Header/Header.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import * as S from './Header.styled'
+
+export function Header() {
+  return (
+    <S.Container>
+      <span>0 xp</span>
+      <span>600 xp</span>
+    </S.Container>
+  )
+}
